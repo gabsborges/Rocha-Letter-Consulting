085e04ef522e28b7c2048e8922387492dba56c56
gabsborges <gabriel.borges_96@hotmail.com>
2022-11-09 09:24:54 -0300

    testando paginacao

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import React,{ useEffect } from 'react';
 import './App.css';
-<<<<<<< HEAD
 import { createBrowserRouter } from 'react-router-dom';
-=======
-import { BrowserRouter, Routes, Route} from "react-router-dom";
->>>>>>> e2b68bce88d476b3e7777239c3c69409b88a4fc3
 import Home from './pages/Home';
 import ThanksYouPage from './pages/ThankYouPage';
 import NonPage from './pages/NonPage';
 
-<<<<<<< HEAD
 const router = createBrowserRouter([
   {
     path: '/',
@@ -27,27 +21,3 @@ const router = createBrowserRouter([
 ])
 
 export { router }
-=======
-function App() {
-  useEffect (()=>{
-    const script = document.createElement("script");
-    script.type = "text/javascript";
-    script.src="https://paginas.rocks/scripts/capture/capture.js";
-    document.body.appendChild(script);
-    return()=>{
-      document.body.removeChild(script);
-    };
-},[])
-  return (
-    <div className="App">
-    <BrowserRouter>
-      <Routes>
-          <Route exact path="/" element={<Home/>} />
-          <Route path="/obrigado" element={<ThanksYouPage/>} />
-      </Routes>
-    </BrowserRouter>
-    </div>
-  );
-}
-export default App;
->>>>>>> e2b68bce88d476b3e7777239c3c69409b88a4fc3
